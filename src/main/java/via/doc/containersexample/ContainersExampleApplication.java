c36b25d084177337b56cd51439a07047bcfdef6e
Ameya Mahankal <ameya.mahankal@gmail.com>
2023-11-10 16:55:57 +0100

    KubDeploy

--- src/main/java/via/doc/containersexample/ContainersExampleApplication.java
+++ src/main/java/via/doc/containersexample/ContainersExampleApplication.java
@@ -5,19 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class ContainersExampleApplication {
 
-    @RequestMapping("/")
-    public String index()
-    {
-        return "<body><h1>Containerization and Orchestration</h1>" +
-                "<h2>Dockerizing Spring Boot Backend Application </h2>" +
-                "<p>With docker, we can containerize SEP4 backend and frontend apps. </p></body>";
-    }
-    public static void main(String[] args) {
-        SpringApplication.run(ContainersExampleApplication.class, args);
-    }
+	@RequestMapping()
+	public String index() {
+		return "<body> <h1> Containerization and Orchestration </h1> </body>";
+	}
+	public static void main(String[] args) {
+
+		SpringApplication.run(ContainersExampleApplication.class, args);
+	}
 
 }
--- src/test/java/via/doc/containersexample/ContainersExampleApplicationTests.java
+++ src/test/java/via/doc/containersexample/ContainersExampleApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ContainersExampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
